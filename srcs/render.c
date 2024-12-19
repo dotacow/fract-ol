5ef2889584d784e590ea4811d87eeaf2bbcbb39e
dotacow <dotacowidkmynamesry@gmail.com>
2024-12-19 19:29:46 +0300

    norm

--- srcs/render.c
+++ srcs/render.c
@@ -6,7 +6,7 @@
 /*   By: yokitane <yokitane@student.42amman.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/18 17:23:51 by dotacow           #+#    #+#             */
-/*   Updated: 2024/12/19 18:12:04 by yokitane         ###   ########.fr       */
+/*   Updated: 2024/12/19 19:28:41 by yokitane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,7 +54,6 @@ void	fractal_render(t_data *data)
 	int	x;
 	int	y;
 
-	fprintf(stderr,"%lf current zoom",data->zoom);
 	y = -1;
 	while (++y < HEIGHT)
 	{
